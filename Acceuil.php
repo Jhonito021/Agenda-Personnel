e2deeeb9113a4291a3b8a0ab32deb62453142c02
Jhonito021 <jhonito021@gmail.com>
2025-06-29 18:52:37 +0300

    Add files via upload

---
+++ Acceuil.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/animation.css">
+    <link rel="stylesheet" href="CSS/style_sombre.css">
+</head>
+<body>
+    <div class="container">
+        <div class="home_text">
+            <h1 class="home_text_left">
+                Agenda <span class="professionel">Professionel</span>
+            </h1>
+            
+            <p class="paragraphe" style="color: white; margin-bottom: 10px;">
+                Lorem ipsum dolor, sit amet consectetur 
+                adipisicing elit. Expedita aperiam ex eum <br>aut beatae ipsum cumque, 
+                officiis totam consectetur voluptates, 
+                fugit nam quae assumenda? Quod adipisci nam earum alias debitis!
+            </p>
+
+            <button id="connecter"><a href="se_connecter.php">Se connecter</a></button>
+            <button id="inscrire"><a href="s_inscrire.php">Créer un compte </a></button>
+        </div>
+
+        <div class="img_logo_right">
+            <img src="image/metallica.jpg" alt="image" class="logo_right">
+        </div>
+    </div>
+    <button id="btnChange">yoh</button>
+    <script src="JavaScript/script_sombre.js"></script>
+</body>
+</html>
---
+++ script_sombre.js
@@ -0,0 +1,7 @@
+if (localStorage.getItem ("theme") === "dark") {
+    document.body.classList.add ("dark");
+}
+document.getElementById ("btnChange").addEventListener ("click", () => {
+    document.body.classList.toggle ("dark");
+    localStorage.setItem ("theme", document.body.classList.contains("dark") ? "dark" : "light");
+});
